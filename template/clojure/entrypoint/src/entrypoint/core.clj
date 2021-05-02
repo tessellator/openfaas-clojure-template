a3b394589661c99b750be9701e0cc05800794784
Chad Taylor <chadtaylor@hey.com>
2021-05-02 11:48:27 -0500

    Delete lock file when Jetty is shutting down

--- template/clojure/entrypoint/src/entrypoint/core.clj
+++ template/clojure/entrypoint/src/entrypoint/core.clj
@@ -5,7 +5,8 @@
            [java.io File]))
 
 (defn add-lifecycle-listener
-  "Creates a file /tmp/.lock when Jetty is started.
+  "Creates a file /tmp/.lock when Jetty is started and deletes it when Jetty is
+   shutting down.
 
   This is associated with a healthcheck. The existence of the /tmp/.lock file
   indicates a successful healthcheck and that the container running Jetty is
@@ -16,13 +17,14 @@
                            (lifeCycleFailure [_this _event _cause])
 
                            (lifeCycleStarted [_this _event]
-                             (.createNewFile (java.io.File. "/tmp/.lock")))
+                             (.createNewFile (File. "/tmp/.lock")))
 
                            (lifeCycleStarting [_this _event])
 
                            (lifeCycleStopped [_this _event])
 
-                           (lifeCycleStopping [_this _event])))
+                           (lifeCycleStopping [_this _event]
+                             (.delete (File. "/tmp/.lock")))))
   server)
 
 (defn -main [& args]
