be8ef38a78835d02d3f42114e643270eb6410eb3
Vincenzo Chianese <vincenz.chianese@icloud.com>
2021-09-27 14:49:21 -0500

    set up clojure start correctly

--- template/clojure/entrypoint/build.clj
+++ template/clojure/entrypoint/build.clj
@@ -1,16 +1,12 @@
 (ns build
   (:require [clojure.tools.build.api :as b]))
 
-(def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
-(def uber-file "target/Entrypoint.jar")
 
 (defn uber [_]
-  (b/copy-dir {:src-dirs ["src" "resources"]
-               :target-dir class-dir})
   (b/compile-clj {:basis basis
                   :src-dirs ["src"]
-                  :class-dir class-dir})
-  (b/uber {:class-dir class-dir
-           :uber-file uber-file
+                  :class-dir "target/classes"})
+  (b/uber {:class-dir "target/classes"
+           :uber-file "bin/Entrypoint.jar"
            :basis basis}))
